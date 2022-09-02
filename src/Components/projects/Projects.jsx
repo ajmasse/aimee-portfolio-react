2846105007fb785853e3eff3fbe3c30bdc58f70d
Aimee <ajmasse68@gmail.com>
2022-09-02 13:30:05 -0600

    Added sections for header, about, and skills
    - Coded new sections of portfolio
    - Added responsive styles and altered general ones in index.css

--- src/Components/projects/Projects.jsx
+++ src/Components/projects/Projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./projects.css";
 
 const Projects = () => {
-  return <section id="projects"></section>;
+  return <section id="projects">Projects</section>;
 };
 
 export default Projects;
