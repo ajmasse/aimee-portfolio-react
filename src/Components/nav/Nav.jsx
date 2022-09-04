8c184cc38714e703ebb1d059d9c63d9b91ce8648
Aimee <ajmasse68@gmail.com>
2022-09-04 11:37:56 -0600

    Added navigation
    - Added navigation bar at bottom of viewport
    - Added react icons
    - Responsive styles

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "./nav.css";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BsGrid } from "react-icons/bs";
+import { TbMessages } from "react-icons/tb";
+import { GiBrain } from "react-icons/gi";
+import { BsPersonCircle } from "react-icons/bs";
+import { useState } from "react";
 
-const Nav = () => {
-  return <section id="nav">Nav</section>;
+export const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome /> <span>Home</span>
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <BsPersonCircle /> <span>About</span>
+      </a>
+      <a
+        href="#skills"
+        onClick={() => setActiveNav("#skills")}
+        className={activeNav === "#skills" ? "active" : ""}
+      >
+        <GiBrain /> <span>Skills</span>
+      </a>
+      <a
+        href="#projects"
+        onClick={() => setActiveNav("#projects")}
+        className={activeNav === "#projects" ? "active" : ""}
+      >
+        <BsGrid /> <span>Projects</span>
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <TbMessages /> <span>Contact</span>
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
