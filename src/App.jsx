106576b2b4bfe921f91a194e617e8edc76d7e30d
Aimee <ajmasse68@gmail.com>
2022-09-06 14:21:49 -0600

    Compressed images, edited styles, added script for analytics
    - Compressed majority of images for quicker load time and less weight
    - Edited styles -> fixed margin issue causing misalignment in skills section
    - Added tag for google analytics -> tracking site users

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Skills from "./Components/skills/Skills";
 import Projects from "./Components/projects/Projects";
 import Contact from "./Components/contact/Contact";
 import Footer from "./Components/footer/Footer";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "G-QP21NNGEC8";
+ReactGA.initialize(TRACKING_ID);
 
 export const App = () => {
   return (
